0e3d863a3d344cff77e1dca17959787a32558df5
Sebastian Lozada <sebaslozaare@gmail.com>
2024-04-24 21:05:07 -0600

    version 2

--- MainSuma.java
+++ MainSuma.java
@@ -1,20 +1,21 @@
 import java.util.Scanner;
-public class MainSuma{ 
-    public static void main(String[] args){
+
+public class MainSuma {
+    public static void main(String[] args) {
         Scanner op = new Scanner(System.in);
-        int y,x;
+        int y, x;
 
-         System.out.println("ingresa un numero entero");
+        System.out.println("ingresa un numero entero");
 
-         x=op.nextInt();
+        x = op.nextInt();
 
-         System.out.println("ingresa otro numero entero");
+        System.out.println("ingresa otro numero entero");
 
-         y=op.nextInt();
+        y = op.nextInt();
 
-         Suma evaluar = new Suma();
-         evaluar. x= x;
-         evaluar.y= y;
+        Suma evaluar = new Suma();
+        evaluar.x = x;
+        evaluar.y = y;
         evaluar.mostrarResultado();
     }
 
